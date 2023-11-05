044e17311ccfc46b756becf1f5ad051358dfda04
ibrahimarashid <ibrahim.justonemore@gmail.com>
2023-11-05 15:59:01 +0800

    removed blueprints.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
 from models import db
-from blueprints import create_app
+from routes.auth import auth_bp
+from routes.concerts import concerts_bp
+from routes.views import views_bp
+from routes.profile import profile_bp
+from routes.purchase import purchase_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,7 +17,11 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-main_app = create_app()
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(concerts_bp, url_prefix='/concerts')
+app.register_blueprint(profile_bp, url_prefix='/profile')
+app.register_blueprint(purchase_bp, url_prefix='/purchase')
+app.register_blueprint(views_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
