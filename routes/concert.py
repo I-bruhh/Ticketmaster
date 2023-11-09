01d9b7b769f647ecc30d2337d149eb03d7917cb1
Nexecis <nexecis@gmail.com>
2023-11-09 19:00:57 +0800

    security patch part 1

--- routes/concert.py
+++ routes/concert.py
@@ -1,22 +1,18 @@
 from flask import Blueprint, render_template
-from routes.concert_db import concert_db_bp
-import requests
+import routes.concert_db as concert_db_bp
 
 concerts_bp = Blueprint('concerts', __name__)
-
-base_url = 'http://127.0.0.1:5000'
 
 
 @concerts_bp.route('/concerts')
 def concerts():
-    concerts_data = requests.get(f'{base_url}/concerts').json()
-    print(concerts_data)
+    concerts_data = concert_db_bp.get_all_concerts().get_json()
     return render_template('concerts.html', concerts=concerts_data)
 
 
 @concerts_bp.route('/concert/<int:concert_id>')
 def concert_detail(concert_id):
-    concert = requests.get(f'{base_url}/concerts/{concert_id}').json()
+    concert = concert_db_bp.get_concert_by_id(concert_id)
     if concert:
         return render_template('concert_detail.html', concert=concert)
     else:
--- routes/concert_db.py
+++ routes/concert_db.py
@@ -9,8 +9,6 @@ dynamodb = boto3.client('dynamodb')
 # Define the DynamoDB table name for Concerts
 concerts_table_name = 'Concert'
 
-
-@concert_db_bp.route('/concerts', methods=['GET'])
 def get_all_concerts():
     try:
         # Access the DynamoDB table to list all concerts
@@ -38,8 +36,6 @@ def get_all_concerts():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-
-@concert_db_bp.route('/concert/<int:concert_id>', methods=['GET'])
 def get_concert_by_id(concert_id):
     try:
         # Access the DynamoDB table to get a specific concert by concert_id
@@ -53,8 +49,18 @@ def get_concert_by_id(concert_id):
         )
         item = response.get('Item')
         if item:
-            concert = dict((k, v['S']) for k, v in item.items())
-            return jsonify(concert)
+            formatted_item = {
+                "concert_id": item["concert_id"]["N"],
+                "name": item["name"]["S"],
+                "dates": [date["S"] for date in item["dates"]["L"]],
+                "venues": [venue["S"] for venue in item["venues"]["L"]],
+                "categories": [int(category["N"]) for category in item["categories"]["L"]],
+                "start_ticket_sale": item["start_ticket_sale"]["S"],
+                "end_ticket_sale": item["end_ticket_sale"]["S"],
+                "limit_per_person": int(item["limit_per_person"]["N"]),
+                "total_tickets_for_sale": int(item["total_tickets_for_sale"]["N"]),
+            }
+            return formatted_item
         else:
             return jsonify({'message': 'Concert not found'}), 404
     except Exception as e:
