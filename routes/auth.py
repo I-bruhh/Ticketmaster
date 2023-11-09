ae0f2abee483fa4ea8c67d25daab60c7e144c1dc
Nexecis <nexecis@gmail.com>
2023-11-09 19:47:44 +0800

    security patch part 2

--- routes/auth.py
+++ routes/auth.py
@@ -1,11 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from werkzeug.security import check_password_hash, generate_password_hash
-import requests
+import routes.auth_db as auth_db
 
 auth_bp = Blueprint('auth', __name__)
-
-base_url = 'http://127.0.0.1:5000'
-
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,12 +10,10 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        # Make a GET request to fetch the user data
-        response = requests.get(f'{base_url}/users/{username}')
-
-        if response.status_code == 200:
-            user_data = response.json()
-
+        data, status_code = auth_db.get_user_by_username(username)
+        user_data = data.get_json()
+        
+        if status_code == 200:
             if user_data and check_password_hash(user_data['password'], password):
                 session['username'] = user_data['username']
                 return redirect(url_for('concerts.concerts'))
@@ -34,22 +29,19 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        # Make a GET request to check if the username already exists
-        existing_user = requests.get(f'{base_url}/create_users/{username}')
+        data, status_code = auth_db.get_user_by_username(username)
 
-        if existing_user.status_code == 200:
+        if status_code == 200:
             return "Username already exists"
         else:
             user_data = {
                 'username': username,
                 'password': generate_password_hash(password, method='pbkdf2:sha256')
-                # Add other user attributes as needed
             }
 
-            # Make a POST request to create the user
-            create_user_response = requests.post(f'{base_url}/create_users', json=user_data)
+            data, status_code = auth_db.create_user(user_data)
 
-            if create_user_response.status_code == 200:
+            if status_code == 200:
                 return redirect(url_for('auth.login'))
             else:
                 return "User registration failed"
--- routes/auth_db.py
+++ routes/auth_db.py
@@ -1,7 +1,5 @@
 import boto3
-from flask import Blueprint, jsonify, request
-
-auth_db_bp = Blueprint('auth_db', __name__)
+from flask import jsonify
 
 # Initialize the DynamoDB client
 dynamodb = boto3.client('dynamodb')
@@ -9,19 +7,6 @@ dynamodb = boto3.client('dynamodb')
 # Define the DynamoDB table name for the User model
 user_table_name = 'User'
 
-
-@auth_db_bp.route('/users', methods=['GET'])
-def get_users():
-    try:
-        response = dynamodb.scan(TableName=user_table_name)
-        items = response.get('Items', [])
-        users = [dict((k, v['S']) for k, v in item.items()) for item in items]
-        return jsonify(users)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-@auth_db_bp.route('/users/<string:username>', methods=['GET'])
 def get_user_by_username(username):
     try:
         response = dynamodb.get_item(
@@ -32,22 +17,20 @@ def get_user_by_username(username):
         item = response.get('Item')
         if item:
             user = dict((k, v['S']) for k, v in item.items())
-            return jsonify(user)
+            return jsonify(user), 200
         else:
             return jsonify({'error': 'User not found'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@auth_db_bp.route('/create_users', methods=['POST'])
-def create_user():
+def create_user(user_data):
     try:
-        user_data = request.json
         dynamodb.put_item(
             TableName=user_table_name,
             Item={k: {'S': str(v)} for k, v in user_data.items()}
         )
-        return jsonify({'message': 'User created successfully'})
+        return jsonify({'message': 'User created successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
