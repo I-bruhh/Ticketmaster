410bc53fe45e7268b494317a4ed107754aefc288
ibrahimarashid <ibrahim.justonemore@gmail.com>
2023-11-10 16:29:11 +0800

    entering booth

--- routes/fairness.py
+++ routes/fairness.py
@@ -10,3 +10,11 @@ def waiting_room(concert_id, queue_position):
     selected_concert = concert_db.get_concert_by_id(concert_id)
 
     return render_template('waiting_room.html', concert=selected_concert, queue_position=queue_position)
+
+
+@fairness_bp.route('/concert/<int:concert_id>/booth')
+def booth(concert_id):
+    selected_concert = concert_db.get_concert_by_id(concert_id)
+
+    # Handle GET request to render the booth page
+    return render_template("booth.html", concert=selected_concert)
