917e14f2b5ee9d96d667e73613d48b7247f9e107
ibrahimarashid <ibrahim.justonemore@gmail.com>
2023-11-11 16:36:14 +0800

    first final

--- routes/auth_db.py
+++ routes/auth_db.py
@@ -7,10 +7,11 @@ dynamodb = boto3.client('dynamodb')
 # Define the DynamoDB table name for the User model
 USER_TABLE_NAME = 'User'
 
+
 def get_user_by_username(username):
     try:
         response = dynamodb.get_item(
-            TableName = USER_TABLE_NAME,
+            TableName=USER_TABLE_NAME,
             Key={'username': {'S': username}
                  }
         )
@@ -27,11 +28,9 @@ def get_user_by_username(username):
 def create_user(user_data):
     try:
         dynamodb.put_item(
-            TableName = USER_TABLE_NAME,
+            TableName=USER_TABLE_NAME,
             Item={k: {'S': str(v)} for k, v in user_data.items()}
         )
         return jsonify({'message': 'User created successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-# Add more authentication-related routes
